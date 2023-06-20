3d6f76266527249f82be35a4c2178024afe46c47
Claire Pasquiou <clairepasquiou@hotmail.com>
2023-06-20 11:23:11 +0200

    Added the Github repo URL

--- src/App.js
+++ src/App.js
@@ -4,8 +4,20 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
-
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a target="_blank" href="https://willowy-centaur-da4a04.netlify.app/">
+          Claire Pasquiou
+        </a>{" "}
+        and is
+        <a
+          href="https://github.com/ClairePasquiou/react-weather-app-final"
+          target="_blank">
+          {" "}
+          open-sourced on Github
+        </a>
+      </footer>
     </div>
   );
 }
