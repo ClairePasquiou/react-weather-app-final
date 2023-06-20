635521e4ae272fe85497f08571223032fe4b713f
Claire Pasquiou <clairepasquiou@hotmail.com>
2023-06-20 23:08:18 +0200

    Added CSS styling

--- src/Weather.js
+++ src/Weather.js
@@ -14,7 +14,12 @@ export default function Weather() {
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              autoFocus="on"
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -23,15 +28,21 @@ export default function Weather() {
         <li>Tuesday 15:28</li>
         <li>Mostly Cloudy</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="Mostly cloudy"
-          />
-          6°C
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="Mostly cloudy"
+              className="float-left"
+            />
+            <div className="float-left">
+              <span className="temperature">6</span>
+              <span className="unit">°C</span>
+            </div>
+          </div>
         </div>
-        <div className="col-6">
+        <div className="col-6 description">
           <ul>
             <li>Precipitation: 15%</li>
             <li>Humidity: 72%</li>
