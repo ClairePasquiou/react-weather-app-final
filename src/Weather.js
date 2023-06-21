6d23ee009955d482f14a3866ce7407966c6873b0
Claire Pasquiou <clairepasquiou@hotmail.com>
2023-06-21 17:38:50 +0200

    added the correct icon URL

--- src/Weather.js
+++ src/Weather.js
@@ -13,7 +13,7 @@ export default function Weather(props) {
       humidity: response.data.main.humidity,
       date: new Date(response.data.dt * 1000),
       description: response.data.weather[0].description,
-      icon: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+      icon: ` https://openweathermap.org/img/wn/${response.data.weather[0].icon}.png `,
       wind: response.data.wind.speed,
       city: response.data.name,
     });
